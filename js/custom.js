521511ad3c2c060aa9af4b4121789293de6acbf9
msg215 <59672294+msg215@users.noreply.github.com>
2020-10-11 20:06:18 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,14 @@
 
 /* dark mode selector */
-function darkModeToggle() {
+/* function darkModeToggle() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+} */
+
+function enableStylesheet (node) {
+  node.rel = '/css/light.css';
+}
+
+function disableStylesheet (node) {
+  node.rel = '/css/dark.css';
 }
