1fe6fc57b9ba1cf43ad42c27788889da0cb5fe58
msg215 <59672294+msg215@users.noreply.github.com>
2020-10-11 20:38:56 -0400

    update js

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,34 @@
   element.classList.toggle("dark-mode");
 } */
 
-function lightMode (node) {
+function enableStylesheet (node) {
   node.rel = '/css/light.css';
 }
 
-function darkMode (node) {
+function disableStylesheet (node) {
   node.rel = '/css/dark.css';
+}
+
+function removeExternalStyleSheetLink(cssLinkId) {
+  var cssLink = document.getElementById(cssLinkId);
+  cssLink.parentNode.removeChild(cssLink);
+}
+ 
+function createExternalStyleSheetLink(cssLinkId, href) {
+  var cssLink = document.createElement("link");
+  cssLink.id = cssLinkId;
+  cssLink.type = "text/css";
+  cssLink.rel = "stylesheet";
+  cssLink.href = href;
+  cssLink.media = "screen";
+  document.getElementsByTagName("head")[0].appendChild(cssLink);
+}
+
+ 
+function changeStyleSheet(newThemeName) {
+  var gCurThemeName = "mono";
+  removeExternalStyleSheetLink(gCurThemeName);
+  createExternalStyleSheetLink(newThemeName, newThemeName + ".css");
+  gCurThemeName = newThemeName;
+  document.getElementById("using").innerHTML = gCurThemeName + ".css";
 }
