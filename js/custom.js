084acc6a3ffe9f7410214a6fe3a2fc0a78bfdf75
msg215 <59672294+msg215@users.noreply.github.com>
2020-10-12 21:37:35 -0400

    update dark theme

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,5 @@
 
-/* dark mode selector */
-/* function darkModeToggle() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-} */
-
-function enableStylesheet(node) {
+/* function enableStylesheet(node) {
   node.rel = 'stylesheet';
 }
 
@@ -19,59 +13,53 @@ function removeExternalStyleSheetLink(cssLinkId) {
 }
 
 function createExternalStyleSheetLink(cssLinkId, href) {
-  console.log('msg')
   var cssLink = document.createElement("link");
   cssLink.id = cssLinkId;
   cssLink.type = "text/css";
   cssLink.rel = "stylesheet";
   cssLink.href = href;
   cssLink.media = "screen";
-  /*   document.getElementsByTagName("head")[0].appendChild(cssLink);
-  
-  
-    document.getElementById('theme').setAttribute('href', cssLinkId); */
+    document.getElementsByTagName("head")[0].appendChild(cssLink);
+    document.getElementById('theme').setAttribute('href', cssLinkId);
   localStorage.setItem("sheet", cssLinkId);
-
 }
-
-function changeStyleSheet() {
-
-
-  /*   if (theme == "light") {
-      document.getElementById("main-msg-logo").src = "/img/msg_dark.png"
-      newTheme = "dark"
-    } else {
-      newTheme = "light"
-      document.getElementById("main-msg-logo").src = "//img/msg_light.png"
-    }
-    removeExternalStyleSheetLink(theme);
-    createExternalStyleSheetLink(newTheme, "/css/" + newTheme + ".css");
-    theme = newTheme; */
-}
-
+ */
 function applyStyleSheet(sheet) {
   document.getElementById('themeToggle').setAttribute('href', sheet);
   localStorage.setItem("sheet", sheet);
+
+  if (window.location.href == "/index.html") {
+    if (sheet == "/css/dark.css") {
+      document.getElementById("main-msg-logo").src = "/img/msg_dark.png"
+    } else {
+      document.getElementById("main-msg-logo").src = "/img/msg_light.png"
+    }
+
+  }
+
 }
-
-
 function swapStyleSheet() {
   var currentCSS = document.getElementById('themeToggle').href
-  console.log('msg', currentCSS)
   if (currentCSS == "/css/dark.css") {
-    console.log('msg sheet', currentCSS)
     var sheet = "/css/light.css"
     document.getElementById('themeToggle').setAttribute('href', sheet);
     localStorage.setItem("sheet", sheet);
 
-  } else if (currentCSS = "/css/light.css") {
-    console.log('msg sheet', currentCSS)
+  } else if (currentCSS = "//css/light.css") {
     var sheet = "/css/dark.css"
     document.getElementById('themeToggle').setAttribute('href', sheet);
     localStorage.setItem("sheet", sheet);
   }
-}
 
+  if (window.location.href == "/index.html") {
+    if (sheet == "/css/dark.css") {
+      document.getElementById("main-msg-logo").src = "/img/msg_dark.png"
+    } else {
+      document.getElementById("main-msg-logo").src = "/img/msg_light.png"
+    }
+
+  }
+}
 
 window.onload = _ =>
   applyStyleSheet(
